3a327a9d8bef338358dcae8882581ac4798eac1e
Rahul3229 <r-ilipilla@wiu.edu>
2025-03-03 18:46:08 -0500

    V1.3(LG)

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -7,6 +7,7 @@ import Lottie from "lottie-react";
 import Marquee from "react-fast-marquee";
 import {ReactTyped} from 'react-typed';
 import {useState, useEffect} from 'react';
+import AWSBadge from "../Assets/AWS-badge.png";
 export default function Landing()
 {
 
@@ -80,8 +81,8 @@ export default function Landing()
                 <Lottie animationData={Section2SVG} loop={true} />
                 </div>
                 <div className="Section2Text">
-                <h1 className="font2 Section2Heading">
-                    GETTING TO KNOW ME:<br /><span className="Section2Font font2">A LIFE IN TECH</span>
+                <h1 className="font2 SectionHeading">
+                    GETTING TO KNOW ME:<br /><span className="SectionSubHeading font2">A LIFE IN TECH</span>
                     <p className="font3 Section2SubText">
                 Walter Mitty (Ben Stiller), an employee at Life magazine, spends day after monotonous day developing photos for the publication. To escape the tedium, Walter inhabits a world of exciting daydreams in which he is the undeniable hero. Walter fancies a fellow employee named Cheryl (Kristen Wiig) and would love to date her, but he feels unworthy. However, he gets a chance to have a real adventure when Life's new owners send him on a mission to obtain the perfect photo for the final print issue.
                 </p>
@@ -95,14 +96,13 @@ export default function Landing()
             <div className="section3">
             <div className="section3Text">
                <div className="font2 section3Box">
-                <h1 className="section3Heading">PROUDLY CERTIFIED:<br /><span className="font2 section3Font">AWS DEVELOPER ASSOCIATE</span>
-                <p className="section3Subtext font3"> Walter Mitty (Ben Stiller), an employee at Life magazine, spends day after monotonous day developing photos for the publication. To escape the tedium, Walter inhabits a world of exciting daydreams in which he is the undeniable hero. Walter fancies a fellow employee named Cheryl (Kristen Wiig) and would love to date her, but he feels unworthy. However, he gets a chance to have a real adventure when Life's new owners send him on a mission to obtain the perfect photo for the final print issue.</p>
+                <h1 className="SectionHeading">PROUDLY CERTIFIED:<br /><span className="font2 SectionSubHeading">AWS DEVELOPER ASSOCIATE</span>
+                <p className="section3Subtext font3"> Walter Mitty (Ben Stiller), an employee at Life magazine, spends day after monotonous day developing photos for the publication. To escape the tedium, Walter inhabits a world of exciting daydreams in which he is the undeniable hero. Walter fancies a fellow employee named Cheryl (Kristen Wiig) and would love to date her, but he feels unworthy. </p>
                 </h1>
-                
                </div>
             </div>
             <div className="section3SVG">
-
+            <img src={AWSBadge} className="ImgPng"></img>
             </div>
             </div>
 
